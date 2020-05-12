45a542c77b8330a2a1468b66e20a5aab71ac0cb2
Sapna <sana.bnsl@gmail.com>
2020-05-12 16:50:51 +0530

    Initial commit

---
+++ DemoFramework/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  DemoFramework
+//
+//  Created by Sapna Bansal on 12/05/20.
+//  Copyright © 2020 Sapna Bansal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Hello Welcome to the user, here we see how to use the framwork.")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '11.0'
+
+target 'DemoFramework' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DemoFramework
+    pod 'MBProgressHUD', '~> 1.1.0', :modular_headers => true
+end
